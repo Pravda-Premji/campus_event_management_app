c9fb0a757e5e98f5e3c0d3ada7433fe4528d345e
Vandana52004 <vandanakunnil@gmail.com>
2026-02-13 23:43:45 +0530

    Connected event logic to Firestore

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -1,23 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class EventService {
 
-  // Create Event
   Future<void> createEvent(String title, String description) async {
     if (title.isEmpty || description.isEmpty) {
       print("Fields cannot be empty");
       return;
     }
 
-    print("Event Created: $title");
+    await FirebaseFirestore.instance.collection("events").add({
+      "title": title,
+      "description": description,
+      "createdAt": Timestamp.now(),
+    });
+
+    print("Event stored in Firestore");
   }
 
-  // Register Event
   Future<void> registerEvent(String eventId, String userId) async {
     if (eventId.isEmpty || userId.isEmpty) {
       print("Invalid registration");
       return;
     }
 
-    print("User $userId registered for $eventId");
-  }
+    await FirebaseFirestore.instance.collection("registrations").add({
+      "eventId": eventId,
+      "userId": userId,
+      "registeredAt": Timestamp.now(),
+    });
 
+    print("Registration stored in Firestore");
+  }
 }
+
